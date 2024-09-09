b793541c8af2ec1aacee2866a4ab9c2b685c68e9
Tatyana N <mairon.angband@gmail.com>
2024-09-09 19:23:57 +0300

    fix games

--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -4,14 +4,12 @@ import gameValue, { checkDivider } from '../index-games.js';
 const descriptionGcd = 'Find the greatest common divisor of given numbers.';
 
 const getQuestionAndResult = () => {
-  const randomNumberOne = _.random(1, 100);
-  const randomNumberTwo = _.random(1, 100);
+  const [randomNumberOne, randomNumberTwo] = [_.random(1, 100), _.random(1, 100)];
+  const sameDividerNum = [];
 
   const findDividerNumberOne = checkDivider(randomNumberOne);
-
   const findDividerNumberTwo = checkDivider(randomNumberTwo);
 
-  const sameDividerNum = [];
   for (let m = 0; m < findDividerNumberOne.length; m += 1) {
     for (let k = 0; k < findDividerNumberTwo.length; k += 1) {
       if (findDividerNumberOne[m] === findDividerNumberTwo[k]) {
@@ -27,6 +25,7 @@ const getQuestionAndResult = () => {
 
   return [questionGcd, resultGcd];
 };
+
 export default () => {
   gameValue(descriptionGcd, getQuestionAndResult);
 };
