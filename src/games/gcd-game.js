8ee528bc1216bf50d5e68069f5014d042ba9b4a1
Tatyana N <mairon.angband@gmail.com>
2024-09-09 17:07:07 +0300

    fix games

--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -2,9 +2,11 @@ import _ from 'lodash';
 import gameOption, { checkDivider } from '../index-games.js';
 
 const descriptionGame = 'Find the greatest common divisor of given numbers.';
+
+const randomNumberOne = _.random(1, 100);
+const randomNumberTwo = _.random(1, 100);
+
 const getQuestionAndResult = () => {
-  const randomNumberOne = _.random(1, 100);
-  const randomNumberTwo = _.random(1, 100);
   const questionGame = `${randomNumberOne} ${randomNumberTwo}`;
 
   const resultOne = checkDivider(randomNumberOne);
